81afeb294e25b1ddff6c17429372131cbc6c8110
plinde1 <150308533+plinde1@users.noreply.github.com>
2024-10-22 08:20:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -155,28 +155,6 @@ marker.addListener ("click", () => {
     createObjsMarkers(objs);
     createWhMarkers(wh);
 
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: getPoints(),
-    map: map,
-  });
-  var heatmap2 =  new google.maps.visualization.HeatmapLayer({
-    data: getPoints2(),
-    map: map,
-    gradient: ["rgba(0, 255, 255, 0)",
-    "rgba(0, 255, 255, 1)",
-    "rgba(0, 191, 255, 1)",
-    "rgba(0, 127, 255, 1)",
-    "rgba(0, 63, 255, 1)",
-    "rgba(0, 0, 255, 1)",
-    "rgba(0, 0, 223, 1)",
-    "rgba(0, 0, 191, 1)",
-    "rgba(0, 0, 159, 1)",
-    "rgba(0, 0, 127, 1)",
-    "rgba(63, 0, 91, 1)",
-    "rgba(127, 0, 63, 1)",
-    "rgba(191, 0, 31, 1)",
-    "rgba(255, 0, 0, 1)",]
-  });
   document.getElementById("toggle-createObjsMarkers").addEventListener("click", createObjsMarkers);
   document.getElementById("toggle-createWhMarkers").addEventListener("click", createWhMarkers);
 
