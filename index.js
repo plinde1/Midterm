72aedb00043f99a9d34cc22d75112c26353518b3
plinde1 <150308533+plinde1@users.noreply.github.com>
2024-10-22 13:10:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let map, heatmap;
+let map, objMarkers = [], whMarkers = [];
 
 var objs = [  
   
@@ -104,7 +104,7 @@ async function initMap() {
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
     const myLatlng = { lat: 41.3114, lng: -105.5911  };
 
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
       center: myLatlng,
       mapId: "ded0469314e2c640",
@@ -127,6 +127,7 @@ async function initMap() {
       infoWindow.setContent(marker.title);
       infoWindow.open(marker.map, marker);
     });
+    objMarkers.push(marker);
   });
 };
 
@@ -146,56 +147,24 @@ marker.addListener ("click", () => {
   infoWindow.setContent(marker.title);
   infoWindow.open(marker.map, marker);
 });
+whMarkers.push(marker);
 });
 };
-    createObjsMarkers(objs);
-    createWhMarkers(wh);
+    createObjsMarkers();
+    createWhMarkers();
 
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: getPoints(),
-    map: map,
-  });
-  var heatmap2 =  new google.maps.visualization.HeatmapLayer({
-    data: getPoints2(),
-    map: map,
-    gradient: ["rgba(0, 255, 255, 0)",
-    "rgba(0, 255, 255, 1)",
-    "rgba(0, 191, 255, 1)",
-    "rgba(0, 127, 255, 1)",
-    "rgba(0, 63, 255, 1)",
-    "rgba(0, 0, 255, 1)",
-    "rgba(0, 0, 223, 1)",
-    "rgba(0, 0, 191, 1)",
-    "rgba(0, 0, 159, 1)",
-    "rgba(0, 0, 127, 1)",
-    "rgba(63, 0, 91, 1)",
-    "rgba(127, 0, 63, 1)",
-    "rgba(191, 0, 31, 1)",
-    "rgba(255, 0, 0, 1)",]
-  });
-  document.getElementById("toggle-heatmap").addEventListener("click", toggleHeatmap);
-  document.getElementById("toggle-heatmap2").addEventListener("click", toggleHeatmap2);
+  document.getElementById("toggle-objects").addEventListener("click", toggleObjsMarkers);
+  document.getElementById("toggle-wh").addEventListener("click", toggleWhMarkers);
+  
+function toggleObjsMarkers() {
+  objMarkers.forEach(marker => {
+    marker.map = marker.map ? null : map;
+});
 
-function toggleHeatmap() {
-  heatmap.setMap(heatmap.getMap() ? null : map);
+function toggleWhMarkers() {
+  whMarkers.forEach(marker => {
+    marker.map = marker.map ? null : map;
+});
 }
-function toggleHeatmap2() {
-  heatmap2.setMap(heatmap2.getMap() ? null : map);
-}
-function getPoints() {
-    var points = []
-    for(var i=0; i<objs.length; i++){
-        points.push(new google.maps.LatLng(objs[i].lat, objs[i].lon))
-    }
-  return points
-}
-function getPoints2() {
-    var points = []
-    for(var i=0; i<wh.length; i++){
-        points.push(new google.maps.LatLng(wh[i].lat, wh[i].lon))
-    }
-  return points
-}
-
 }
 
